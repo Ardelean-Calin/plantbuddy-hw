e7d39bfc2cd624611fc7e780dd1c5ecf81071e86
Calin <ardelean.calin@outlook.com>
2021-02-05 20:10:25 +0200

    Switched to nRF52810

--- 03_Firmware/Inc/UserConfig.h
+++
@@ -1,8 +0,0 @@
-#ifndef USERCONFIG_H__
-#define USERCONFIG_H__
-
-#include "BlueNRG1_gpio.h"
-#include "BlueNRG1_i2c.h"
-#include "BlueNRG1_sysCtrl.h"
-
-#endif
--- 03_Firmware/Inc/ble.h
+++
@@ -1,8 +0,0 @@
-#ifndef BLE_H__
-#define BLE_H__
-
-#define BLE_TASK_PERIOD_MS 10
-
-void vBLEInit(void);
-
-#endif
