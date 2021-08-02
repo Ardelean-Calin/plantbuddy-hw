400f11bb2929736e6967ae98e8d3ba1423ae4779
Ardelean Călin Petru <ardelean.calin@outlook.com>
2021-08-02 17:14:45 +0300

    Backup before migration to GitLAB

---
+++ 02_Software/plantbuddy/src/views/LandingPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div>Landing Page {{ count }}</div>
+    <button @click="increment">Increment</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class LandingPage extends Vue {
+  @Prop() private count = 0;
+
+  increment(): void {
+    this.count++;
+  }
+}
+</script>
+
+<style scoped></style>
