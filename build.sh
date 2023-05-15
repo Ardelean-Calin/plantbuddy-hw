bf3f5ed73b352f25945f69f3715544dca845c576
Ardelean Calin <artech@duck.com>
2023-05-15 15:21:58 +0300

    Corrected build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
-podman build -t kikit .
+# podman build -t kikit . # Run this once on a new machine.
 podman run -it -w /kikit -v $(pwd):/kikit:Z kikit fab jlcpcb --no-drc --assembly --schematic PlantBuddy.kicad_sch PlantBuddy.kicad_pcb output/
