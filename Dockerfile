884ecd7df20dcef8aff585e964670ba6600a91bf
Ardelean Calin <artech@duck.com>
2023-05-15 11:45:28 +0300

    Ordered boards

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM yaqwsx/kikit:nightly-v7
+
+RUN apt update && apt install -y fonts-firacode fonts-jetbrains-mono
--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/bash
 
-podman run -it -w /kikit -v $(pwd):/kikit:Z yaqwsx/kikit:nightly-v7 fab jlcpcb --no-drc --assembly --schematic PlantBuddy.kicad_sch PlantBuddy.kicad_pcb output/
+podman build -t kikit .
+podman run -it -w /kikit -v $(pwd):/kikit:Z kikit fab jlcpcb --no-drc --assembly --schematic PlantBuddy.kicad_sch PlantBuddy.kicad_pcb output/
