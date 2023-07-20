531be4611459c40900f8eae74d18d85d14b2f404
Ardelean Calin <9417983+Ardelean-Calin@users.noreply.github.com>
2023-07-20 13:57:49 +0300

    Corrected build.sh and ordered 5ps

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 
-podman run -it -w /kikit -v $(pwd):/kikit:Z yaqwsx/kikit kikit fab jlcpcb --no-drc --assembly --schematic PlantBuddy.kicad_sch PlantBuddy.kicad_pcb output/
+podman run -it -w /kikit -v $(pwd):/kikit:Z docker.io/yaqwsx/kikit:v1.3.0-v7 fab jlcpcb --no-drc --assembly --schematic PlantBuddy.kicad_sch PlantBuddy.kicad_pcb output/
